b835f58a91c9b6f7e650eac4c2d115adf6c81d44
baschenbrenner <baschenbrenner@hotmail.com>
2022-11-03 10:19:39 -0500

    finished

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,20 @@
+import ipdb
+
 def oxford_comma(items):
-    return None
+    counter = 1
+    new_list=[]
+    if len(items) == 1:
+        return items[0]
+    if len(items) == 2:
+        return items[0] + " and " + items[1]
+    for item in items:
+        
+        if (counter == len(items)):
+            new_list.append("and " + item)
+        else:
+            new_list.append(item+",")
+            counter +=1
+    
+    
+
+    return " ".join(new_list)
